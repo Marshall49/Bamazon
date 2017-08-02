4b4e635736bfd5b4c9b6b44e9bce7f6113d5becc
Malcolm Strong <strong.malcolm@yahoo.com>
2017-08-02 08:52:24 -0400

    Finished 98% of the first challenge

--- Bamazon.js
+++ Bamazon.js
@@ -29,66 +29,98 @@ function allShoes() {
 function Question() {
 inquirer.prompt ([
   {
-    type: 'Shoe',
-    name: 'What',
+    type: 'list',
+    name: 'Shoe',
     message: "What Shoes are You Looking to Buy?",
-    choices: {
-      shoeName: "Nike",
-      indexNumber: 1
-    }
-    // {
-    //   shoeName: "Adidas",
-    //   indexNumber: 2
-    // },
-    // {
-    //   shoeName: "New Balance",
-    //   indexNumber: 3
-    // }
-    // {
-    //   shoeName: "Reebok",
-    //   indexNumber: 4
-    // }
-    // {
-    //   shoeName: "Puma",
-    //   indexNumber: 5
-    // },
-    // {
-    //   shoeName: "Asics",
-    //   indexNumber: 6
-    // },
-    // {
-    //   shoeName: "Gucci",
-    //   indexNumber: 7
-    // },
-    // {
-    //   shoeName: "Air Jordans",
-    //   indexNumber: 8
-    // },
-    // {
-    //   shoeName: "Saucony",
-    //   indexNumber: 9
-    // },
-    // {
-    //   shoeName: "Timbaland",
-    //   indexNumber: 10
-    // }
+    choices: [{
+      name: "Nike",
+      value: 1
+    },
+    {
+      name: "Adidas",
+      value: 2
+    },
+    {
+      name: "New Balance",
+      value: 3
+    },
+    {
+      name: "Reebok",
+      value: 4
+    },
+    {
+      name: "Puma",
+      value: 5
+    },
+    {
+      name: "Asics",
+      value: 6
+    },
+    {
+      name: "Gucci",
+      value: 7
+    },
+    {
+      name: "Jordans",
+      value: 8
+    },
+    {
+      name: "Saucony",
+      value: 9
+    },
+    {
+      name: "Timbaland",
+      value: 10
+    }]
   },
 
+
+
 ]).then(function(answers){
-  connection.query("SELECT * FROM products", function(err, res) {
-    var userInput = JSON.stringify(answers.What, null, 2);
-    console.log(userInput);
-    if (userInput ===  answers.choices, null, 2) {
-    console.log("The User chose");
-  }
-  else {
-    console.log("Nope");
-  }
+  var quantity;
+  var id = answers.Shoe;
+  connection.query("SELECT * FROM products WHERE ?",
+  { id: id }, function(err, res) {
+    //svar userInput = JSON.stringify(answers.name);
+    var response = res[0];
+    console.log("--------------------")
+    console.log(`You've selected ${response.shoe}!`)
+    //console.log("The quantity is " + response.quantity)
+    quantity = response.quantity;
+    inquirer.prompt ([
+      {
+        name: 'numberOfPairs',
+        message: "How many Pairs are You Looking to Buy?",
+      }
+    ]).then(function(answer) {
+      console.log(quantity);
+      console.log("You want to buy: " + answer.numberOfPairs);
+      var updatedQuantity = quantity - answers.numberOfPairs;
+      console.log("updatedQuantity " + updatedQuantity);
+      if (answer.numberOfPairs <= quantity) {
+        connection.query("UPDATE products SET quantity = ? WHERE ?", [updatedQuantity, { id: id }],  function(err, res) {
+          if (err) throw err;// var response = res[0];
+          console.log("We Got It!");
+          console.log(res);
+        });
+      } else {
+          console.log("We are Out!");
+        }
+
+      //}
+    });
 
 
-})
-})
+  })
+    })
+  //   if (userInput ===  answers.name) {
+  // }
+
+
+
 }
+
+
 
 
 
